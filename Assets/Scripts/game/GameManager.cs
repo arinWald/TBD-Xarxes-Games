a3645b3e69e0a1eea0a81ebd6722663cd1211535
Arnau González Acosta <arnicraft@gmail.com>
2024-11-18 17:31:53 +0100

    more changes

--- Assets/Scripts/game/GameManager.cs
+++ Assets/Scripts/game/GameManager.cs
@@ -15,11 +15,15 @@ public class GameManager : MonoBehaviour
     public GameObject clientTextObj;
     TextMeshProUGUI clientText;
 
+    public GameObject winTextGameObject;
+    TextMeshProUGUI winText;
+
     // Start is called before the first frame update
     void Start()
     {
         serverText = serverTextObj.GetComponent<TextMeshProUGUI>();
         clientText = clientTextObj.GetComponent<TextMeshProUGUI>();
+        winText = winTextGameObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -27,5 +31,16 @@ public class GameManager : MonoBehaviour
     {
         clientText.text = clientScore.ToString();
         serverText.text = serverScore.ToString();
+
+        if (serverScore >= 5)
+        {
+            winTextGameObject.SetActive(true);
+            winText.text = "Right Player Wins!";
+        }
+        else if (clientScore >= 5)
+        {
+            winTextGameObject.SetActive(true);
+            winText.text = "Left Player Wins!";
+        }
     }
 }
