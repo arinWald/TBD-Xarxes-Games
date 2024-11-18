2244edb35807b43bb5182fe99082208c20650c2a
Arnau González Acosta <arnicraft@gmail.com>
2024-11-18 13:53:35 +0100

    game finish

--- Assets/Scripts/Network/GameStateManager.cs
+++ Assets/Scripts/Network/GameStateManager.cs
@@ -7,12 +7,26 @@ using TMPro;
 
 public class GameStateManager : MonoBehaviour
 {
-    private float send_horizontalInput = 0f;
-    private float send_verticalInput = 0f;
-    private bool send_spaceInput = false;
-    private bool send_spaceInputUp = false;
-    public bool send_spaceInputDown = false;
-    private bool send_shiftInput = false;
+    public struct PlayerInputData
+    {
+        public float send_horizontalInput;
+        public float send_verticalInput;
+        public bool send_spaceInput;
+        public bool send_spaceInputUp;
+        public bool send_spaceInputDown;
+        public bool send_shiftInput;
+
+        public PlayerInputData(float h, float v, bool space, bool spaceUp, bool spaceDown, bool shift)
+        {
+            send_horizontalInput = h;
+            send_verticalInput = v;
+            send_spaceInput = space;
+            send_spaceInputUp = spaceUp;
+            send_spaceInputDown = spaceDown;
+            send_shiftInput = shift;
+        }
+    };
+
 
     [HideInInspector]
     public ClientUDP clientUDPScript;
@@ -21,6 +35,9 @@ public class GameStateManager : MonoBehaviour
 
     public PlayerMovement localPlayer;
     public PlayerMovement remotePlayer;
+
+    PlayerInputData incomingData;
+    PlayerInputData pendingToSendData;
 
 
     private void Start()
@@ -36,8 +53,12 @@ public class GameStateManager : MonoBehaviour
     // Assign the received controls
     void SetPlayerControls()
     {
-        remotePlayer.horizontalInput = send_horizontalInput;
-        remotePlayer.verticalInput = send_verticalInput;
+        remotePlayer.horizontalInput = incomingData.send_horizontalInput;
+        remotePlayer.verticalInput = incomingData.send_verticalInput;
+        remotePlayer.spaceInput = incomingData.send_spaceInput;
+        remotePlayer.spaceInputUp = incomingData.send_spaceInputUp;
+        remotePlayer.spaceInputDown = incomingData.send_spaceInputDown;
+        remotePlayer.shiftInput = incomingData.send_shiftInput;
     }
 
     // Send current controls
