472b22c5a46d2236162d31ce257f39389d05adc7
Arnau González Acosta <arnicraft@gmail.com>
2024-12-19 00:21:43 +0100

    world replication fix

--- Assets/Scripts/Network/GameStateManager.cs
+++ Assets/Scripts/Network/GameStateManager.cs
@@ -13,37 +13,39 @@ public class GameStateManager : MonoBehaviour
     [Serializable]
     public struct PlayerInputData
     {
+        // Bytes = 
         public float send_horizontalInput;
         public float send_verticalInput;
+        public float send_rotationAngle;
         public bool send_spaceInput;
         public bool send_spaceInputUp;
         public bool send_spaceInputDown;
         public bool send_shiftInputDown;
-        public float send_rotationAngle;
 
         public PlayerInputData(float h, float v, bool space, bool spaceUp, bool spaceDown, bool shiftDown, float rotAng)
         {
             send_horizontalInput = h;
             send_verticalInput = v;
+            send_rotationAngle = rotAng;
             send_spaceInput = space;
             send_spaceInputUp = spaceUp;
             send_spaceInputDown = spaceDown;
             send_shiftInputDown = shiftDown;
-            send_rotationAngle = rotAng;
         }
     };
 
     [Serializable]
     public struct WorldState
     {
-        public Vector3 _playerPos;
+        public Vector3 _serverPos;
+        public Vector3 _clientPos;
         public Vector3 _ballPos;
 
-        public WorldState(Vector3 ppos, Vector3 bpos)
+        public WorldState(Vector3 spos, Vector3 cpos, Vector3 bpos)
         {
-            _playerPos = ppos;
+            _serverPos = spos;
+            _clientPos = cpos;
             _ballPos = bpos;
-
         }
     };
 
@@ -62,6 +64,9 @@ public class GameStateManager : MonoBehaviour
 
     PlayerInputData incomingData;
     PlayerInputData pendingToSendData;
+
+    WorldState worldStateData;
+    bool setWorldData;
 
     bool IAmClient;
 
@@ -85,18 +90,12 @@ public class GameStateManager : MonoBehaviour
         Thread inGameSend = new Thread(SendPlayerControls);
         inGameSend.Start();
 
-        if (IAmClient && clientUDPScript != null)
-        {
-            Thread inGameWorldReceive = new Thread(RecieveWorldState);
-            inGameWorldReceive.Start();
-        }
     }
 
     private void Update()
     {
         SetPlayerControls();
 
-        Debug.Log("TITOLAAAAAAAAAAAAAAAAAAAAAAA");
 
         if (server)
         {
@@ -106,10 +105,14 @@ public class GameStateManager : MonoBehaviour
 
                 SendWorldState();
             }
-            else
-            {
-                replicationTimer += Time.deltaTime;
-            }
+            replicationTimer += Time.deltaTime;
+        }
+
+        //Debug.Log("Set world Data? " + setWorldData);
+        if(setWorldData && IAmClient)
+        {
+            setWorldData = false;
+            SetWorldData();
         }
 
        //Debug.Log("timer: " + replicationTimer);
@@ -149,12 +152,12 @@ public class GameStateManager : MonoBehaviour
 
             if (IAmClient && clientUDPScript != null)
             {
-                Debug.Log("Controls sending to client");
+                //Debug.Log("Controls sending to client");
                 clientUDPScript.socket.SendTo(buffer, SocketFlags.None, clientUDPScript.Remote);
             }
             else if (serverUDPScript != null)
             {
-                Debug.Log("Controls sending to client");
+                //Debug.Log("Controls sending to client");
                 serverUDPScript.socket.SendTo(buffer, SocketFlags.None, serverUDPScript.Remote);
             }
         }
@@ -176,8 +179,22 @@ public class GameStateManager : MonoBehaviour
                 byte[] trimmedData = new byte[recv];
                 Array.Copy(data, trimmedData, recv);
 
-                // Deserialize the received data into a struct
-                incomingData = GameStateManager.BytesToStruct<GameStateManager.PlayerInputData>(trimmedData);
+                // Check the package size to differentiate
+                if (trimmedData.Length == Marshal.SizeOf(typeof(WorldState)))
+                {
+                    worldStateData = BytesToStruct<WorldState>(trimmedData);
+
+                    if (!worldStateData.Equals(default(WorldState)))
+                    {
+                        setWorldData = true;
+                        //Debug.Log("Receiving World Data as a Client");
+                    }
+                }
+                else if(trimmedData.Length == Marshal.SizeOf(typeof(PlayerInputData)))
+                {
+                    // Deserialize the received data into a struct
+                    incomingData = BytesToStruct<PlayerInputData>(trimmedData);
+                }                
             }
         }
         else if (serverUDPScript != null)
@@ -211,7 +228,10 @@ public class GameStateManager : MonoBehaviour
     {
         int size = Marshal.SizeOf(typeof(T));
         if (bytes.Length != size)
+        {
+            Debug.LogError($"Received bytes length ({bytes.Length}) does not match struct size ({Marshal.SizeOf(typeof(T))}).");
             throw new ArgumentException("Byte array size does not match struct size.");
+        }
 
         IntPtr ptr = Marshal.AllocHGlobal(size); 
         Marshal.Copy(bytes, 0, ptr, size);       
@@ -223,7 +243,13 @@ public class GameStateManager : MonoBehaviour
 
     public void SendWorldState()
     {
-        WorldState data = new WorldState(localPlayer.gameObject.transform.position, localPlayer.ball.gameObject.transform.position);
+        Debug.Log("Send world state");
+
+        WorldState data = new WorldState(localPlayer.gameObject.transform.position,
+                                        remotePlayer.gameObject.transform.position,
+                                        localPlayer.ball.gameObject.transform.position);
+        //WorldState data = new WorldState(localPlayer.ball.gameObject.transform.position.x,
+        //                        localPlayer.ball.gameObject.transform.position.z);
 
         byte[] buffer = StructToBytes(data);
 
@@ -237,27 +263,14 @@ public class GameStateManager : MonoBehaviour
         }
     }
 
-    public void RecieveWorldState()
+    private void SetWorldData()
     {
-        while (true)
-        {
-            // Allocate a buffer to receive data
-            byte[] data = new byte[1024];
-            int recv = clientUDPScript.socket.ReceiveFrom(data, ref clientUDPScript.Remote);
-
-            // Trim the byte array to match the received data length
-            byte[] trimmedData = new byte[recv];
-            Array.Copy(data, trimmedData, recv);
-
-            // Deserialize the received data into a struct
-            //SetWorld Data
-            SetWorldData(GameStateManager.BytesToStruct<GameStateManager.WorldState>(trimmedData));
-        }
+        localPlayer.gameObject.transform.position = worldStateData._clientPos;
+        localPlayer.ball.gameObject.transform.position = worldStateData._ballPos;
+        remotePlayer.gameObject.transform.position = worldStateData._serverPos;
+        Debug.Log("Setting World Data: \n" + worldStateData._serverPos + "\n" +
+            worldStateData._ballPos + "\n" + worldStateData._clientPos);
     }
 
-    private void SetWorldData(WorldState data)
-    {
-        localPlayer.gameObject.transform.position = data._playerPos;
-        localPlayer.ball.gameObject.transform.position = data._ballPos;
-    }
+
 }
--- Assets/Scripts/game/GameManager.cs
+++ Assets/Scripts/game/GameManager.cs
@@ -60,7 +60,7 @@ public class GameManager : MonoBehaviour
 
     public void GoalReset()
     {
-            ResetPos();
+        ResetPos();
     }
 
     private void ResetPos()
